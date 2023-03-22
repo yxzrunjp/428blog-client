71215252b87f9383c2a451a874f2886d2ea39bf3
yxzrunjp <yxz894894@gmail.com>
2023-03-22 17:19:20 +0800

    添加404页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,72 +5,80 @@ const routes = [
         path: '/',
         name: 'Layout',
         component: () => import('@/views/Layout.vue'),
-        redirect:'/index',
+        redirect: '/index',
         children: [
             {
                 path: 'index',
-                name:'Index',
-                meta:{
-                    title:'博客'
+                name: 'Index',
+                meta: {
+                    title: '博客'
                 },
                 component: () => import('@/views/Index.vue'),
             },
             {
                 path: 'blogDetail/:blogId',
-                name:'BlogDetail',
-                meta:{
-                    title:'博客详情'
+                name: 'BlogDetail',
+                meta: {
+                    title: '博客详情'
                 },
                 component: () => import('@/views/BlogDetail.vue'),
             },
             {
-                path:'category',
-                name:"Category",
-                meta:{
-                    title:'分类专栏'
+                path: 'category',
+                name: "Category",
+                meta: {
+                    title: '分类专栏'
                 },
                 component: () => import('@/views/Category.vue'),
             },
             {
-                path:'category/:categoryId',
-                name:"CategoryDetail",
-                meta:{
-                    title:'分类专栏'
+                path: 'category/:categoryId',
+                name: "CategoryDetail",
+                meta: {
+                    title: '分类专栏'
                 },
                 component: () => import('@/views/CategoryDetail.vue'),
             },
             {
-                path:'special',
-                name:"Special",
-                meta:{
-                    title:'专题'
+                path: 'special',
+                name: "Special",
+                meta: {
+                    title: '专题'
                 },
                 component: () => import('@/views/Special.vue'),
             },
             {
-                path:'special/:id',
-                name:"SpecialDetail",
-                meta:{
-                    title:'专题'
+                path: 'special/:id',
+                name: "SpecialDetail",
+                meta: {
+                    title: '专题'
                 },
                 component: () => import('@/views/SpecialDetail.vue'),
             },
             {
-                path:'user',
-                name:"User",
-                meta:{
-                    title:'成员'
+                path: 'user',
+                name: "User",
+                meta: {
+                    title: '成员'
                 },
                 component: () => import('@/views/User.vue'),
             },
             {
-                path:'message',
-                name:"Message",
-                meta:{
-                    title:'留言板'
+                path: 'message',
+                name: "Message",
+                meta: {
+                    title: '留言板'
                 },
                 component: () => import('@/views/Message.vue'),
             },
+            {
+                path: '/:pathMatch(.*)*',
+                name: 'NotFound',
+                meta: {
+                    title: '404'
+                },
+                component: () => import('@/views/NotFound.vue')
+            }
         ],
     }
 ]
---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="not-found">
+        <el-empty description="当前页面不存在" :image-size="400"></el-empty>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+    .el-empty {
+        &:deep(.el-empty__description) {
+            p {
+                font-size: 30px;
+                color: #ff4757;
+            }
+        }
+    }
+
+
+}
+</style>
