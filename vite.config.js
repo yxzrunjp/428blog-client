86fc2eac5fe2685f7c2fe74b13d23164e02c1973
yxzrunjp <yxz894894@gmail.com>
2023-03-31 16:00:25 +0800

    配置第三方依赖打包

--- vite.config.js
+++ vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (!id.includes('node_modules')) {
-            console.log(id);
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         },
         entryFileNames: 'static/[name].[hash].js',
